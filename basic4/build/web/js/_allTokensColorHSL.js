f5ebd2ab193523fb3f137d4c5b830fbbaf64e7c1
Sandeep Upadhyay <165522048+mxsan5555@users.noreply.github.com>
2024-09-20 15:59:52 -0400

    feat: getting all tokens with color filter

---
+++ basic4/build/web/js/_allTokensColorHSL.js
@@ -0,0 +1,23 @@
+/**
+ * Do not edit directly, this file was auto-generated.
+ */
+
+export const baseRed100 = "hsl(0, 100%, 98%)";
+export const baseRed200 = "hsl(0, 95%, 92%)";
+export const baseRed300 = "hsl(0, 97%, 85%)";
+export const baseRed400 = "hsl(0, 95%, 75%)";
+export const baseRed500 = "hsl(0, 88%, 68%)";
+export const baseRed600 = "hsl(0, 76%, 57%)";
+export const baseRed700 = "hsl(0, 61%, 48%)";
+export const baseRed800 = "hsl(0, 56%, 39%)";
+export const colorsBlack = "hsl(0, 0%, 0%)";
+export const colorsWhite = "hsl(0, 0%, 100%)";
+export const colorsOrange100 = "hsl(40, 100%, 97%)";
+export const colorsOrange200 = "hsl(39, 96%, 89%)";
+export const colorsOrange300 = "hsl(38, 93%, 77%)";
+export const colorsOrange400 = "hsl(33, 90%, 65%)";
+export const colorsOrange500 = "hsl(27, 84%, 57%)";
+export const colorsOrange600 = "hsl(24, 75%, 50%)";
+export const colorsOrange700 = "hsl(20, 71%, 44%)";
+export const colorsOrange800 = "hsl(16, 65%, 37%)";
+export const colorsOrange900 = "hsl(14, 61%, 30%)";
---
+++ basic4/build/web/js/_colorHEX.js
@@ -0,0 +1,23 @@
+/**
+ * Do not edit directly, this file was auto-generated.
+ */
+
+export const baseRed100 = "#fff5f5";
+export const baseRed200 = "#fed7d7";
+export const baseRed300 = "#feb2b2";
+export const baseRed400 = "#fc8181";
+export const baseRed500 = "#f56565";
+export const baseRed600 = "#e53e3e";
+export const baseRed700 = "#c53030";
+export const baseRed800 = "#9b2c2c";
+export const colorsBlack = "#000000";
+export const colorsWhite = "#ffffff";
+export const colorsOrange100 = "#fffaf0";
+export const colorsOrange200 = "#feebc8";
+export const colorsOrange300 = "#fbd38d";
+export const colorsOrange400 = "#f6ad55";
+export const colorsOrange500 = "#ed8936";
+export const colorsOrange600 = "#dd6b20";
+export const colorsOrange700 = "#c05621";
+export const colorsOrange800 = "#9c4221";
+export const colorsOrange900 = "#7b341e";
