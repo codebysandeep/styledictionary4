f4e90b9ea4600640162e76bbabc45aa253b3fbd6
Sandeep Upadhyay <165522048+mxsan5555@users.noreply.github.com>
2024-09-20 00:36:44 -0400

    feat: (basic4) introduce clean files

---
+++ basic4/build.js
@@ -0,0 +1,14 @@
+import StyleDictionary from "style-dictionary";
+
+// Create custom TransformGroup
+
+export default {
+  hooks: {
+    formats: {},
+    transforms: {},
+  },
+  source: ["tokens/**/*.@(json|json5)"],
+  platforms: {
+    
+  },
+};
