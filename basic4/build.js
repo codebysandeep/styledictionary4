70fce7888778ae7b46267cabccc710c499eb079d
Sandeep Upadhyay <165522048+mxsan5555@users.noreply.github.com>
2024-09-20 00:41:00 -0400

    feat: basic4 - added custom transforms and transform groups with config.json

--- basic4/build.js
+++ basic4/build.js
@@ -1,14 +1,32 @@
-import StyleDictionary from "style-dictionary";
+import StyleDictionary from 'style-dictionary';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
 
-// Create custom TransformGroup
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+console.log('Build started...');
+console.log('\n==============================================');
+
+// REGISTER THE CUSTOM TRANSFORMS
+
+// REGISTER THE CUSTOM TRANSFORM GROUPS
+console.log(StyleDictionary.transformGroup['group_name']);
 
 export default {
   hooks: {
     formats: {},
     transforms: {},
-  },
-  source: ["tokens/**/*.@(json|json5)"],
-  platforms: {
-    
-  },
+  }
 };
+
+
+// APPLY THE CONFIGURATION
+// IMPORTANT: the registration of custom transforms
+// needs to be done _before_ applying the configuration
+const sd = new StyleDictionary(__dirname + '/config.json');
+
+// FINALLY, BUILD ALL THE PLATFORMS
+await sd.buildAllPlatforms();
+
+console.log('\n==============================================');
+console.log('\nBuild completed!');
