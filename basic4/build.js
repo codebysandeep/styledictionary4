bc63fff1402e6dcf6e594456ddce68519befef67
Sandeep Upadhyay <165522048+mxsan5555@users.noreply.github.com>
2024-09-20 14:16:13 -0400

    feat: basic4 - added js all tokens

--- basic4/build.js
+++ basic4/build.js
@@ -8,9 +8,39 @@ console.log('Build started...');
 console.log('\n==============================================');
 
 // REGISTER THE CUSTOM TRANSFORMS
+StyleDictionary.registerTransform({
+  name: "attribute-color",
+  type: "value",
+  transitive: true,
+  filter: (token) => {
+    console.log(`token: ${token.name}, type: ${token.$value}`);
+    return token.type === "color";
+  },
+  transform: (token) => {
+    console.log(`token123: ${token.name}, type123: ${token.$value}`);
+  },
+});
+
+// StyleDictionary.registerFilter({
+//   name: "colors-only",
+//   type: "value",
+//   filter: (token) => {
+//     console.log(`token: ${token.name}, type: ${token.type}`);
+//     return token.type === "color";
+//   },
+// });
+
 
 // REGISTER THE CUSTOM TRANSFORM GROUPS
-console.log(StyleDictionary.transformGroup['group_name']);
+StyleDictionary.registerTransformGroup({
+  name: "css-group",
+  transforms: ["attribute/cti", "name/kebab", "color/hex"],
+});
+
+StyleDictionary.registerTransformGroup({
+  name: "web-group",
+  transforms: ["attribute/cti", "name/camel", "attribute-color"],
+});
 
 export default {
   hooks: {
---
+++ basic4/build/web/js/_number.js
@@ -0,0 +1,6 @@
+/**
+ * Do not edit directly, this file was auto-generated.
+ */
+
+export const textLineHeightsNormal = 1.2;
+export const textLineHeightsLarge = 1.8;
